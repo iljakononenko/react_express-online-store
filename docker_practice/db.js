494eb217149cd8751462ea429fc9e637ae1a65a4
elijah <iljakononenko3@gmail.com>
2023-01-09 13:26:55 +0100

    removed docker_practise and set up a docker-compose to host nginx

--- docker_practice/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const {db} = require('./configuration')
-const {mongo} = require("mongoose");
-
-module.exports.connectDb = () => {
-    mongoose.connect(db, {useNewUrlParser: true})
-
-    return mongoose.connection;
-};
